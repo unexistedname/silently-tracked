290925226b9c7cc3395e54623b111c2b19eb0dc5
GabrielRicam <gbrielwiranata@gmail.com>
2026-01-28 14:31:37 +0700

    forgot to add async funtion

--- main.ts
+++ main.ts
@@ -32,43 +32,90 @@ if (isNaN(interval)) {
 }
 
 // ----main stuff----
-console.log("[ MAIN ] Scraping started...");
-for (let key in baseData) {
-  if (baseData.hasOwnProperty(key)) {
-    try {
-      console.log(`[ MAIN ] Processing ${key}...`);
-      const id: string = key.toLowerCase().replace(/\s+/g, "");
-      const res = await axios.get(baseData[key]);
+(async () => {
+  console.log("[ MAIN ] Scraping started...");
+  for (let key in baseData) {
+    if (baseData.hasOwnProperty(key)) {
+      try {
+        console.log(`[ MAIN ] Processing ${key}...`);
+        const id: string = key.toLowerCase().replace(/\s+/g, "");
+        const res = await axios.get(baseData[key]);
 
-      console.log(`[ MAIN ] Successfully executed GET request for ${key}...`);
-      const dataScrape = Scraper.metadataRK(res.data);
-      const chapterScrape = await Scraper.chapter(res.data);
+        console.log(`[ MAIN ] Successfully executed GET request for ${key}...`);
+        const dataScrape = Scraper.metadataRK(res.data);
+        const chapterScrape = await Scraper.chapter(res.data);
 
-      console.log(`[ MAIN ] Scraping complete! working on local data...`);
-      const oldChapter: number[] = chapterStored[id]?.chapter ?? [];
-      let newChapter: number[] = chapterScrape.filter(
-        (x) => !oldChapter.includes(x),
-      );
-      if (newChapter.length === 0) {
-        newChapter = chapterStored[id]?.newChapter;
+        console.log(`[ MAIN ] Scraping complete! working on local data...`);
+        const oldChapter: number[] = chapterStored[id]?.chapter ?? [];
+        let newChapter: number[] = chapterScrape.filter(
+          (x) => !oldChapter.includes(x),
+        );
+        if (newChapter.length === 0) {
+          newChapter = chapterStored[id]?.newChapter;
+        }
+
+        console.log(
+          `[ MAIN ] New data has been stored to temporary variables. Stopping the program before the iteration ends will delete all progress.`,
+        );
+        dataStored[id] = { ...dataScrape, link: baseData[key] };
+        chapterStored[id] = {
+          chapter: chapterScrape,
+          newChapter: newChapter,
+          lastUpdate: new Date().toISOString(),
+        };
+      } catch (error: unknown) {
+        console.error(error);
       }
-
-      console.log(
-        `[ MAIN ] New data has been stored to temporary variables. Stopping the program before the iteration ends will delete all progress.`,
-      );
-      dataStored[id] = {...dataScrape, link: baseData[key]};
-      chapterStored[id] = {
-        chapter: chapterScrape,
-        newChapter: newChapter,
-        lastUpdate: new Date().toISOString(),
-      };
-    } catch (error: unknown) {
-      console.error(error);
     }
   }
-}
-console.log(`[ MAIN ] Tracking done! Saving data into local file (please do not close it yet)...`);
-writeFileSync(chapterPath, JSON.stringify(chapterStored, null, 2), "utf-8");
-writeFileSync(dataPath, JSON.stringify(dataStored, null, 2), "utf-8");
+  console.log(
+    `[ MAIN ] Tracking done! Saving data into local file (please do not close it yet)...`,
+  );
+  writeFileSync(chapterPath, JSON.stringify(chapterStored, null, 2), "utf-8");
+  writeFileSync(dataPath, JSON.stringify(dataStored, null, 2), "utf-8");
 
-console.log(`[ MAIN ] Saving complete!`);
+  console.log(`[ MAIN ] Saving complete!`);
+  console.log("[ MAIN ] Scraping started...");
+  for (let key in baseData) {
+    if (baseData.hasOwnProperty(key)) {
+      try {
+        console.log(`[ MAIN ] Processing ${key}...`);
+        const id: string = key.toLowerCase().replace(/\s+/g, "");
+        const res = await axios.get(baseData[key]);
+
+        console.log(`[ MAIN ] Successfully executed GET request for ${key}...`);
+        const dataScrape = Scraper.metadataRK(res.data);
+        const chapterScrape = await Scraper.chapter(res.data);
+
+        console.log(`[ MAIN ] Scraping complete! working on local data...`);
+        const oldChapter: number[] = chapterStored[id]?.chapter ?? [];
+        let newChapter: number[] = chapterScrape.filter(
+          (x) => !oldChapter.includes(x),
+        );
+        if (newChapter.length === 0) {
+          newChapter = chapterStored[id]?.newChapter;
+        }
+
+        console.log(
+          `[ MAIN ] New data has been stored to temporary variables. Stopping the program before the iteration ends will delete all progress.`,
+        );
+        dataStored[id] = { ...dataScrape, link: baseData[key] };
+        chapterStored[id] = {
+          chapter: chapterScrape,
+          newChapter: newChapter,
+          lastUpdate: new Date().toISOString(),
+        };
+      } catch (error: unknown) {
+        console.error(error);
+      }
+    }
+  }
+  console.log(
+    `[ MAIN ] Tracking done! Saving data into local file (please do not close it yet)...`,
+  );
+  writeFileSync(chapterPath, JSON.stringify(chapterStored, null, 2), "utf-8");
+  writeFileSync(dataPath, JSON.stringify(dataStored, null, 2), "utf-8");
+
+  console.log(`[ MAIN ] Saving complete!`);
+  process.exit(0);
+})();
