68906ed33e09a689c5d27a8ca787b7ead4c33241
unexistedname <gbrielwiranata@gmail.com>
2026-02-10 19:18:35 +0700

    50% done on mangadex but untested

---
+++ src/main.ts
@@ -0,0 +1,100 @@
+import { resolve, join } from "path";
+import axios from "axios";
+import { readFileSync, existsSync, writeFileSync } from "fs";
+import dotenv from "dotenv";
+dotenv.config();
+
+import * as Scraper from "./tracker/scraper.js";
+const now: Date = new Date();
+
+// data: For storing updated data
+// baseData: For storing list of manga to be updated
+// Change the content in baseData if you want to add/remove manga(s)
+function createNewFile(path: string, fileName: string): string {
+  writeFileSync(path, "{}", "utf-8");
+  console.log(`No ${fileName} file detected, creating a new one...`);
+  return "{}";
+}
+const dir = process.env.DATA_DIRECTORY;
+if (!dir) {
+  throw new Error("DATA_DIRECTORY is not defined!");
+}
+const baseDir = resolve(dir);
+const baseDataPath = resolve("./data/baselink.json");
+const dataPath = join(baseDir, "data.json");
+const chapterPath = join(baseDir, "chapter.json");
+const baseData = JSON.parse(readFileSync(baseDataPath, "utf-8"));
+const dataStored = existsSync(dataPath)
+  ? JSON.parse(readFileSync(dataPath, "utf-8") || "{}")
+  : createNewFile(dataPath, "data");
+const chapterStored = existsSync(chapterPath)
+  ? JSON.parse(readFileSync(chapterPath, "utf-8") || "{}")
+  : createNewFile(chapterPath, "chapter");
+
+enum DOMAIN {
+  Rawkuma = "rawkuma.net",
+  Mangadex = "mangadex.org"
+}
+
+
+
+// ----main stuff----
+(async () => {
+  console.log(`[ MAIN | ${now.toLocaleTimeString()} ] Scraping started...`);
+  for (let key in baseData) {
+    if (baseData.hasOwnProperty(key)) {
+      try {
+        console.log(`[ MAIN ] Processing ${key}...`);
+        const id: string = key.toLowerCase().replace(/\s+/g, "");
+
+        const domain: string = baseData[key].split("/")[2]; 
+        switch (domain) {
+          case DOMAIN.Rawkuma:
+            // TODO
+            break;
+        
+          default:
+            throw new Error("Couldn't find domain name: " + domain);
+            break;
+        }
+
+
+
+        const res = await axios.get(baseData[key]);
+
+        console.log(`[ MAIN ] Successfully executed GET request for ${key}...`);
+        const dataScrape = Scraper.metadataRK(res.data);
+        const chapterScrape = await Scraper.chapter(res.data);
+
+        console.log(`[ MAIN ] Scraping complete! working on local data...`);
+        const oldChapter: Number[] = chapterStored[id]?.chapter ?? [];
+        let newChapter: Number[] = chapterScrape.filter(
+          (x) => !oldChapter.includes(x),
+        );
+        if (newChapter.length === 0) {
+          newChapter = chapterStored[id]?.newChapter;
+        }
+
+        console.log(
+          `[ MAIN ] New data has been stored to temporary variables. Stopping the program before the iteration ends will delete all progress.`,
+        );
+        dataStored[id] = { ...dataScrape, link: baseData[key] };
+        chapterStored[id] = {
+          chapter: chapterScrape,
+          newChapter: newChapter,
+          lastUpdate: new Date().toISOString(),
+        };
+      } catch (error: unknown) {
+        console.error(error);
+      }
+    }
+  }
+  console.log(
+    `[ MAIN ] Tracking done! Saving data into local file (please do not close it yet)...`,
+  );
+  writeFileSync(chapterPath, JSON.stringify(chapterStored, null, 2), "utf-8");
+  writeFileSync(dataPath, JSON.stringify(dataStored, null, 2), "utf-8");
+
+  console.log(`[ MAIN | ${now.toLocaleTimeString()} ] Saving complete!`);
+  process.exit(0);
+})();
--- src/tracker/mangadex.ts
+++ src/tracker/mangadex.ts
@@ -2,19 +2,46 @@ import axios from "axios";
 import * as cheerio from "cheerio";
 import { metadata } from "./lib/metadata";
 
+const baseUrl = "https://api.mangadex.org/manga";
+
 // ------ CHAPTER SCRAPING ----------
-export async function chapter(html: string) {
+export async function chapter(url: string) {
     try {
-        const $ = cheerio.load(html);
+        const id = url.split("/")[4];
+        url = `${baseUrl}/${id}/feed`;
+        const res = await axios.get(url);
+        const data = res.data.data;
+        const chapters = data[0]
+            .filter((ch: any) => ['en', 'id']
+                .some((lang) => lang in (ch.attributes.translatedLanguages ?? {})
+                ))
+            .map((ch: any) => ch.attributes.chapter.toString());
     } catch (error: unknown) {
         throw error;
     }
 }
 
 // ----- METADATA SCRAPING ---------
-export async function metadata(html: string) {
+export async function metadata(url: string): Promise<metadata> {
     try {
-        const $ = cheerio.load(html);
+        const id = url.split("/")[4];
+        url = `${baseUrl}/${id}`;
+        const res = await axios.get(url);
+        const data = res.data.data.attributes;
+        const genre = data.tags.map((tag: any) => tag.attributes?.name?.en).filter(Boolean);
+
+        return {
+            title: data.title['ja-ro'],
+            altTitle: data.title['id'] ?? data.title['en'] ?? data.title['ja'] ?? null,
+            author:,
+            artist:,
+            genre: genre,
+            descriptions: data.description.en ?? data.description.ja ?? "",
+            coverURL,
+            src: 'mangadex'
+
+            // todo
+        }
     } catch (error: unknown) {
         throw error;
     }
